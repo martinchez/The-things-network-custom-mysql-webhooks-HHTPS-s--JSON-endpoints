2442242809c7afc177615b4b92627e3712ae001d
martinchez <martinchege1998@gmail.com>
2024-06-06 15:27:55 +0300

    second device deployment

--- BD_config1.php
+++ BD_config1.php
@@ -2,10 +2,10 @@
 define('TIMEZONE','Africa/Nairobi');
 date_default_timezone_set(TIMEZONE);
 
-define("DB_HOST","rs1.rcnoc.com");
-define("DB_USER","anmartco_LoRaDataDB");
+define("DB_HOST","localhost");
+define("DB_USER","mkulimap_LoRaDataDB");
 define("DB_PASSWORD","Martinwaish1.");
-define("DB_DATABASE","anmartco_LoRaDataDB");
+define("DB_DATABASE","mkulimap_LoRaDataDB");
 
 $conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
 
